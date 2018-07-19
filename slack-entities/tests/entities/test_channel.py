78190c1bb92c201c677ef539321f16c7153ba719
p1v2 <pavliuk96@gmail.com>
2018-07-19 16:27:44 +0300

    Setup tests

--- slack-entities/tests/entities/test_channel.py
+++ slack-entities/tests/entities/test_channel.py
@@ -1,16 +1,16 @@
 """
 We need new test framework
 """
+from unittest import TestCase
+from entities.channel import Channel
 
-# from entities.channel import Channel
-#
-#
-# class ChannelTestCase(SimpleTestCase):
-#     def test_get(self):
-#         # Getting channel by name
-#         channel_1 = Channel.get(name="daily_update")
-#
-#         # Getting channel by id
-#         channel_2 = Channel.get(id=channel_1.id)
-#
-#         self.assertEqual(channel_1, channel_2)
+
+class ChannelTestCase(TestCase):
+    def test_get(self):
+        # Getting channel by name
+        channel_1 = Channel.get(name="daily_update")
+
+        # Getting channel by id
+        channel_2 = Channel.get(id=channel_1.id)
+
+        self.assertEqual(channel_1, channel_2)
