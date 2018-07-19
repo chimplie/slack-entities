0ef49927c029e98fe0c55836c6bed7cd77cf8d23
p1v2 <pavliuk96@gmail.com>
2018-07-19 17:17:14 +0300

    Adapt test acc. to the new workspace

--- slack-entities/tests/entities/test_channel.py
+++ slack-entities/tests/entities/test_channel.py
@@ -8,7 +8,7 @@ from entities.channel import Channel
 class ChannelTestCase(TestCase):
     def test_get(self):
         # Getting channel by name
-        channel_1 = Channel.get(name="daily_update")
+        channel_1 = Channel.get(name="general")
 
         # Getting channel by id
         channel_2 = Channel.get(id=channel_1.id)
--- slack-entities/tests/entities/test_user.py
+++ slack-entities/tests/entities/test_user.py
@@ -8,7 +8,7 @@ from entities.user import User
 class UserTestCase(TestCase):
     def test_get_filter(self):
         # Getting user by id
-        user_1 = User.get(id="U2GUL0QF9")
+        user_1 = User.get(id="UBTGY5U05")
 
         # Getting user from list
         user_2 = User.filter(id=user_1.id)[0]
