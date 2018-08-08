189c23454e0f7949a856a97564442d5dd476de78
Oleh Zorenko <lovemybeard@gmail.com>
2018-08-08 13:53:41 +0300

    fix Channel to get data for private channels and multi-party IM (conversations) 0.0.10 release

--- slack_entities/entities/channel.py
+++ slack_entities/entities/channel.py
@@ -2,18 +2,20 @@ from .resource import SlackResource
 
 
 class Channel(SlackResource):
-    def __init__(self, id, name=None, is_channel=None, **kwargs):
+    fetch_api_method = "conversations.info"
+    fetch_all_api_method = "conversations.list"
+
+    def __init__(self, id, name=None, is_channel=None, is_im=None, is_private=None, is_group=None, **kwargs):
         self.id = id
         self.name = name
         self.is_channel = is_channel
+        self.is_direct = is_im
+        self.is_private = is_private
+        self.is_group = is_group
 
     def __repr__(self):
-        return f"<Channel #{self.name}>"
-
-    @property
-    def is_direct(self):
-        return self.id.startswith("D")
+        return f"<Channel #{self.name or self.id}>"
 
     @property
     def is_public(self):
-        return self.id.startswith("C")
+        return not self.is_private
