70e37540f4cc6ac29a0a5d24fe8ec0a8a459cf14
oleh <lovemybeard@gmail.com>
2018-10-20 15:27:03 +0300

    get user id from SLACK_USER_ID env var in test_user.py

--- slack_entities/tests/entities/test_user.py
+++ slack_entities/tests/entities/test_user.py
@@ -1,14 +1,17 @@
 """
 We need new tests framework
 """
+import os
 from unittest import TestCase
 from slack_entities.entities.user import User
 
 
 class UserTestCase(TestCase):
     def test_get_filter(self):
+        user_id = os.environ.get('SLACK_USER_ID')
+
         # Getting user by id
-        user_1 = User.get(id="UBTGY5U05")
+        user_1 = User.get(id=user_id)
 
         # Getting user from list
         user_2 = User.filter(id=user_1.id)[0]
