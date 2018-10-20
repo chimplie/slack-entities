889bf573d164baf02922de4284011036c8247169
oleh <lovemybeard@gmail.com>
2018-10-20 15:24:47 +0300

    fix tests for Team entity

--- slack_entities/tests/entities/test_team.py
+++ slack_entities/tests/entities/test_team.py
@@ -8,10 +8,7 @@ from slack_entities.entities.team import Team
 
 class TeamTestCase(TestCase):
     def test_get(self):
-        team_id = os.environ.get('SLACK_TEAM_ID', 'TBLEMQT2M')
-        team_name = os.environ.get('SLACT_TEAM_NAME', 'UTS Chatbot')
-
-        # Getting team by id
-        team = Team.get(id=team_id)
+        team_name = os.environ.get('SLACK_TEAM_NAME')
+        team = Team.get()
 
         self.assertEqual(team.name, team_name)
