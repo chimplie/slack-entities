166e15be50a2e39208f00b8d9962cbfb5d350588
p1v2 <pavliuk96@gmail.com>
2018-11-07 08:57:06 +0200

    Set 0.0.17 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="slack_entities",
-    version="0.0.16",
+    version="0.0.17",
     author="Oleh Zorenko",
     author_email="oleh@chimplie.com",
     description="Package for more convenient work with Slack API",
