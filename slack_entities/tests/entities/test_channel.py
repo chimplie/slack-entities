972abf0183e867692f2e874f79ff596af16fde0e
oleh <lovemybeard@gmail.com>
2019-10-15 23:28:40 +0300

    Change channel name in test_channel.py.

--- slack_entities/tests/entities/test_channel.py
+++ slack_entities/tests/entities/test_channel.py
@@ -8,7 +8,7 @@ from slack_entities.entities.channel import Channel
 class ChannelTestCase(TestCase):
     def test_get(self):
         # Getting channel by name
-        channel_1 = Channel.get(name="general")
+        channel_1 = Channel.get(name='test')
 
         # Getting channel by id
         channel_2 = Channel.get(id=channel_1.id)
