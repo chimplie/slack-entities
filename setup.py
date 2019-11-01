3f3b965de00cc977624c4368bfe02571117cbba7
Artur Shyshko <arturshyshko@gmail.com>
2019-11-01 16:25:27 +0200

    Enable importing entities on module level

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="slack_entities",
-    version="0.0.35",
+    version="0.0.4",
     author="Oleh Zorenko",
     author_email="oleh@chimplie.com",
     description="Package for more convenient work with Slack API",
---
+++ slack_entities/entities/__init__.py
@@ -0,0 +1,37 @@
+from .action import (
+    Action, action_from_webhook, BaseAction, BlockAction,
+    ButtonAction, DialogSubmissionAction, MessageAction, SelectAction
+)
+from .channel import Channel
+from .dialog import Dialog
+from .event import (
+    Event, event_from_rtm, event_from_webhook,
+    DeletedMessageEvent, EditedMessageEvent, MessageEvent, TeamJoinEvent, UserChangeEvent
+)
+from .incoming_message import IncomingMessage
+from .modal import Modal
+from .outcoming_message import OutcomingMessage
+from .team import Team
+from .user import User
+
+
+__all__ = [
+    Action,
+    BaseAction,
+    BlockAction,
+    EditedMessageEvent,
+    Event,
+    IncomingMessage,
+    MessageAction,
+    MessageEvent,
+    Modal,
+    OutcomingMessage,
+    SelectAction,
+    Team,
+    TeamJoinEvent,
+    User,
+    UserChangeEvent,
+    action_from_webhook,
+    event_from_rtm,
+    event_from_webhook
+]
