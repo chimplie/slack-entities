91a5f8f0101628bca8832cc196c2d9d027c4755c
oleh <lovemybeard@gmail.com>
2020-05-25 16:55:40 +0300

    Convert LOGGER_LEVEL to int

--- slack_entities/client/client.py
+++ slack_entities/client/client.py
@@ -5,7 +5,7 @@ import os
 from slack import WebClient
 
 
-LOGGER_LEVEL = os.environ.get('LOGGER_LEVEL', 15)
+LOGGER_LEVEL = int(os.environ.get('LOGGER_LEVEL', 15))
 logger = logging.Logger("slack_client", level=LOGGER_LEVEL)
 logger.setLevel(LOGGER_LEVEL)
 
