4d5feafe9ed683090add7546f879d5fda433a548
dmytro <kharchenko.dmitriy.n@gmail.com>
2020-12-23 12:52:05 +0200

    Increment lib version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="slack_entities",
-    version="0.1.27",
+    version="0.1.28",
     author="Oleh Zorenko",
     author_email="oleh@chimplie.com",
     description="Package for more convenient work with Slack API",
